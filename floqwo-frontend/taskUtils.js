e933edba1f974dd9b6769cfe174efd0885ffa7b7
Chris Thornton <cjthornton78@gmail.com>
2024-11-07 13:49:06 +0000

    Now include the day in date formatting

--- floqwo-frontend/taskUtils.js
+++ floqwo-frontend/taskUtils.js
@@ -9,15 +9,18 @@ function calculateTimePassedPercentage(createdAt, dueDate) {
   return ((nowTime - createdTime) / (dueTime - createdTime)) * 100;
 }
 
-// Function to format the date into "DD/MM/YYYY, HH.MM"
+// Function to format the date into "Day DD/MM/YYYY, HH.MM"
 function formatDate(dateString) {
   const date = new Date(dateString);
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dayCode = dayNames[date.getDay()];
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  return `${day}/${month}/${year}, ${hours}.${minutes}`;
+  
+  return `${dayCode} ${day}/${month}/${year}, ${hours}.${minutes}`;
 }
 
 // Function to sort tasks based on priority, due date first, and creation date second
